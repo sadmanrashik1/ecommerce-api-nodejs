d3944013b9a8f40f5baa22dbd9b488bea232574e
Sadman <artyomfreeman@outlook.com>
2025-12-16 02:47:26 +0000

    refactored code

---
+++ middleware/authentication.js
@@ -0,0 +1,48 @@
+const CustomError = require("../errors");
+const { isTokenValid, attachCookiesToResponse } = require("../utils");
+const Token = require("../models/Token");
+const authenticateUser = async (req, res, next) => {
+  const { refreshToken, accessToken } = req.signedCookies;
+  try {
+    if (accessToken) {
+      const payload = isTokenValid(accessToken);
+      const { name, userId, role } = payload;
+      req.user = { name, userId, role };
+      return next();
+    }
+
+    const payload = isTokenValid(refreshToken);
+    const existingToken = await Token.findOne({
+      user: payload.user.userId,
+      refreshToken: payload.refreshToken,
+    });
+    if (!existingToken || !existingToken?.isValid) {
+      throw new CustomError.UnauthenticatedError("Authentication invalid1");
+    }
+    attachCookiesToResponse({
+      res,
+      user: payload.user,
+      refreshToken: existingToken.refreshToken,
+    });
+    req.user = payload.user;
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication Invalid2");
+  }
+};
+
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        "Unauthorized to access this route"
+      );
+    }
+    next();
+  };
+};
+
+module.exports = {
+  authenticateUser,
+  authorizePermissions,
+};
---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: process.env.ETHEREAL_USER,
+    pass: process.env.ETHEREAL_PASS,
+  },
+};
